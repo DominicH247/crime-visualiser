586d72f61f90da3f3d0924a2f5b05de90b30a96a
Hy-M <humayraa.m@outlook.com>
2020-02-23 09:06:15 +0000

    improve footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Footer = () => {
-  return <p className="footer-text">Built by Dom &amp; Humayraa 2020 &copy;</p>;
+  return <p className="footer-text">Built by Dom &amp; Humayraa 2020</p>;
 };
 
 export default Footer;
